80360d43e5bd0fba8f77294bcb443f2bd7bc1d1c
vnijs <radiant@rady.ucsd.edu>
2016-06-20 21:00:39 -0700

    Update

--- pkgs.R
+++ pkgs.R
@@ -1,5 +1,5 @@
 pkgs <- c("car", "miniCRAN", "gridExtra", "GPArotation", "psych", "wordcloud",
           "AlgDesign", "knitr", "ggdendro", "lubridate", "ggplot2", "pryr",
           "shiny", "magrittr", "tidyr", "dplyr", "broom", "shiny", "rmarkdown",
-          "shiny","neuralNetTools",
+          "shiny","NeuralNetTools",
           "data.tree", "nnet", "DT")
