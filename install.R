e06b6ec0ebc9d3371b7d8d82d4e4a567a036bbda
vnijs <radiant@rady.ucsd.edu>
2016-06-24 12:36:27 -0700

    install script

--- install.R
+++ install.R
@@ -1,31 +1,50 @@
 repos <- c("https://radiant-rstats.github.io/minicran/", "https://cloud.r-project.org")
 options(repos = c(CRAN = repos))
 
-install.packages("radiant", repos = "https://radiant-rstats.github.io/minicran/", type = 'binary')
-install.packages("webshot", repos = "http://cran.rstudio.com", type = "binary")
-if (Sys.which("phantomjs") == "") eval(parse(text = "webshot::install_phantomjs()"))
+build <- function() {
+	install.packages("radiant", repos = "https://radiant-rstats.github.io/minicran/", type = 'binary')
+	install.packages("webshot", repos = "http://cran.rstudio.com", type = "binary")
+	if (Sys.which("phantomjs") == "") eval(parse(text = "webshot::install_phantomjs()"))
+}
 
 os <- Sys.info()["sysname"]
 
 if (os == "Windows") {
 	if (grepl("Pr", Sys.getenv("R_HOME"))) {
-		stop("It seems you installed R in the Program Files directory. Please uninstall R and install it into C:\\R\\R-3.3.1")
+		rv <- R.Version()
+    rv <- paste(rv$major, rv$minor, sep = ".")
+		stop(paste0("It seems you installed R in the Program Files directory. Please uninstall R and install into C:\\R\\R-",rv))
 	} else {
+		build()
 		install.packages("installr")
-	  installr::install.miktex(ver)
 		installr::install.rstudio()
-		installr::install.MikTex(if (grepl("64",Sys.getenv()["PROCESSOR_IDENTIFIER"])) 64 else 32)
 		installr::install.Rtools()
-	}
-} else if (os == "Darwin") {
 
+	  inp <- readline("To generate PDF reports in Radiant you will need MikTex. This is large download (approx 100MB). Proceed with the install? y/n ")
+	  if (inp %in% c("y","yes","Yes","yes","YES")) {
+		  ver <- if (grepl("64",Sys.getenv()["PROCESSOR_IDENTIFIER"])) 64 else 32
+		  installr::install.MikTex(ver)
+		}
+	}
+
+	cat("Installation on Windows complete. Start Rstudio and select Radiant from the Addins menu to get started")
+} else if (os == "Darwin") {
+	build()
 	tmp <- tempdir()
 	setwd(tmp)
 	download.file("https://download1.rstudio.org/RStudio-0.99.902.dmg","Rstudio.dmg")
 	system("open RStudio.dmg")
-	download.file("http://tug.org/cgi-bin/mactex-download/MacTeX.pkg", "MacTex.pkg")
-	system("open MacTex.pkg")
+
+	inp <- readline("To generate PDF reports in Radiant you will need MacTex. This is a larger download (approx 2GB). Proceed with the install? y/n ")
+	if (inp %in% c("y","yes","Yes","yes","YES")) {
+		download.file("http://tug.org/cgi-bin/mactex-download/MacTeX.pkg", "MacTex.pkg")
+		system("open MacTex.pkg")
+	}
+	cat("Installation on Mac complete. Start Rstudio and select Radiant from the Addins menu to get started")
 } else {
 	stop("Your OS is not currently supported")
 }
+
+
+
 
