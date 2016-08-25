78cdbf7c3de2b2a32084b32b0f5a38a6ebede76a
vnijs <radiant@rady.ucsd.edu>
2016-08-25 11:10:33 -0700

    exclude gitgadget

--- build.R
+++ build.R
@@ -13,6 +13,7 @@ build <- function() {
 
 	## needed for windoze
 	pkgs <- new.packages(lib.loc = .libPaths()[1], repos = 'https://radiant-rstats.github.io/minicran', type = 'binary', ask = FALSE)
+	pkgs <- pkgs[!grepl("gitgadget",pkgs)]
 	if (length(pkgs) > 0) install.packages(pkgs, repos = 'https://radiant-rstats.github.io/minicran', type = 'binary')
 
 	# see https://github.com/wch/webshot/issues/25#event-740360519
