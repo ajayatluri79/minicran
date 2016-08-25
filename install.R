988ea61b1e2ca26f8036edc0930c94a56f513c52
vnijs <radiant@rady.ucsd.edu>
2016-08-25 11:09:19 -0700

    exclude gitgadget

--- install.R
+++ install.R
@@ -13,7 +13,10 @@ build <- function() {
 
 	## needed for windoze
 	pkgs <- new.packages(lib.loc = .libPaths()[1], repos = 'https://radiant-rstats.github.io/minicran', type = 'binary', ask = FALSE)
-	if (length(pkgs) > 0) install.packages(pkgs, repos = 'https://radiant-rstats.github.io/minicran', type = 'binary')
+	pkgs <- pkgs[!grepl("gitgadget",pkgs)]
+	if (length(pkgs) > 0)
+		install.packages(pkgs, repos = 'https://radiant-rstats.github.io/minicran', type = 'binary')
+  }
 
 	# see https://github.com/wch/webshot/issues/25#event-740360519
 	if (is.null(webshot:::find_phantom())) webshot::install_phantomjs()
